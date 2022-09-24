1ef0904abff3fa6ded5f44893846e7846d1ec9b4
wmxpy <wm@mengw.io>
2022-09-24 12:51:41 -0500

    add index export

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@
  * @description Index
  */
 
+export * from "./class/declare";
+export * from "./class/factory";
+export * from "./class/inject";
+export * from "./class/provide";
 export * from "./declare";
 export * from "./exports/factory";
 export * from "./exports/inject";
